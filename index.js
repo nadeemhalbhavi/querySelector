b0db2d105b26d5f4df817c732e3c57aeba75c725
nadeemhalbhavi <nadeem.halbhavi@gmail.com>
2024-02-23 01:09:29 +0530

    adding style done

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 // Write the code as shown in the video below:
 
-// Write answer to the questions asked below:
+
+const mainHeading = document.querySelector('#main-heading');
+mainHeading.style.textAlign = 'center'
+
+const fruits = document.querySelector('.fruits');
+fruits.style.backgroundColor = 'gray'
+fruits.style.listStyleType = 'none'
+fruits.style.margin = '30px'
+fruits.style.padding = '30px'
+fruits.style.width = '50%'
+fruits.style.borderRadius = '5px'
+
+
+
 
 const basketHeading = document.querySelector('#basket-heading');
 basketHeading.style.color = 'brown'
+basketHeading.style.marginLeft = '30px'
+
+const fruitItems1 = document.querySelectorAll('.fruit');
+for (let i=0;i<fruitItems1.length; i++){
+    fruitItems1[i].style.backgroundColor = 'white'
+    fruitItems1[i].style.padding = '10px'
+    fruitItems1[i].style.margin = '10px'
+    fruitItems1[i].style.borderRadius = '5px'
+
+
+    
+}
 
 const fruitItems = document.querySelectorAll('.fruit:nth-child(even)');
 
